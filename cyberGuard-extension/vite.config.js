33338f08b3a6f9d17fe0cf504b6cadb8f505fa10
kk1701 <karankute05@gmail.com>
2025-05-21 22:17:36 +0530

    initial commit

---
+++ cyberGuard-extension/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'public/popup.html'),
+        options: resolve(__dirname, 'src/options/main.jsx'),
+        background: resolve(__dirname, 'src/background/background.js'),
+        content: resolve(__dirname, 'src/content/contentScript.js'),
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
